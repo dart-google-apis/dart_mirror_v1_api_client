521b195770aeffab0f310c7270c955f07d275b54
Adam Singer <financeCoding@gmail.com>
2014-03-22 19:03:55 -0700

    Automated update

--- lib/mirror_v1_api_browser.dart
+++ lib/mirror_v1_api_browser.dart
@@ -8,6 +8,12 @@ import "package:google_mirror_v1_api/mirror_v1_api_client.dart";
 /** API for interacting with Glass users via the timeline. */
 class Mirror extends Client with BrowserClient {
 
+  /** OAuth Scope2: View your location */
+  static const String GLASS_LOCATION_SCOPE = "https://www.googleapis.com/auth/glass.location";
+
+  /** OAuth Scope2: View and manage your Glass timeline */
+  static const String GLASS_TIMELINE_SCOPE = "https://www.googleapis.com/auth/glass.timeline";
+
   final oauth.OAuth2 auth;
 
   Mirror([oauth.OAuth2 this.auth]);
--- lib/mirror_v1_api_console.dart
+++ lib/mirror_v1_api_console.dart
@@ -9,6 +9,12 @@ import "package:google_mirror_v1_api/mirror_v1_api_client.dart";
 /** API for interacting with Glass users via the timeline. */
 class Mirror extends Client with ConsoleClient {
 
+  /** OAuth Scope2: View your location */
+  static const String GLASS_LOCATION_SCOPE = "https://www.googleapis.com/auth/glass.location";
+
+  /** OAuth Scope2: View and manage your Glass timeline */
+  static const String GLASS_TIMELINE_SCOPE = "https://www.googleapis.com/auth/glass.timeline";
+
   final oauth2.OAuth2Console auth;
 
   Mirror([oauth2.OAuth2Console this.auth]);
